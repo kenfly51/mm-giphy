a7e12c7420feed649582361d67dc313b055a4461
Instance Noodle <ntn.f51@gmail.com>
2019-06-27 00:05:32 +0700

    load gifs

--- src/components/GiphyContainer/components/GiphyContainer.js
+++ src/components/GiphyContainer/components/GiphyContainer.js
@@ -1,10 +1,83 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-
 export class GiphyContainer extends Component {
+  state = {
+    gifs: [],
+    isLoading: false,
+    page: 0,
+    hasMore: false
+  }
+
+  componentDidMount() {
+		this.loadGifs()
+  }
+
+  buildQueryParams = (offset) => {
+    const { apiKey, pageSize, rating } = this.props;
+
+    const queryParams = [];
+
+    if (apiKey) {
+			queryParams.push(`apiKey=${apiKey}`)
+    }
+		if (offset) {
+			queryParams.push(`offset=${offset}`)
+    }
+    if (pageSize) {
+			queryParams.push(`limit=${pageSize}`)
+    }
+    if (rating) {
+			queryParams.push(`rating=${rating}`)
+    }
+    
+    return queryParams;
+  }
+
+  buildUrl = (offset) => {
+    const { url } = this.props;
+    const queryParams = this.buildQueryParams(offset);
+    const query = queryParams.join("&");
+
+    return `${url}?${query}`;
+  }
+  
+  loadGifs = (offset) => {
+    const { page, isLoading } = this.state;
+    
+		if (isLoading) {
+			return
+    }
+
+    const url = this.buildUrl(offset);
+
+		this.setState({
+			isLoading: true
+		}, () => {
+      fetch(url, {
+        method: 'get'
+      })
+      .then(res => res.json())
+      .then(response => {
+        let gifs = response.data.map(g => g.images)
+        let hasMore = true
+        const { total_count, count, offset } = response.pagination
+        if (total_count <= count + offset) {
+          hasMore = false
+        }
+  
+        this.setState({
+          gifs: this.state.gifs.concat(gifs),
+          page: page + 1,
+          isLoading: false,
+          hasMore: hasMore
+        })
+      })
+    })
+	}
+
   render() {
-    return <div>{JSON.stringify(this.props)}</div>
+    return <div>{JSON.stringify(this.state)}</div>
   }
 }
 
